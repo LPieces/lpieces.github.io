162163bea4a76e4d69b6efaa58240f8532e7aad4
LPieces <Ljh_Pieces@163.com>
2026-05-12 04:50:34 +0800

    auto-config CNAME for GitHub Pages

--- scripts/copy-fonts.ts
+++ scripts/copy-fonts.ts
@@ -2,6 +2,7 @@ import fs from 'fs-extra'
 
 async function run() {
   await fs.copy('public/assets/fonts', 'dist/assets/fonts', { overwrite: true })
+  await fs.copy('CNAME', 'dist/CNAME', { overwrite: true })
 }
 
 run()
